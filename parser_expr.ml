0dbcf00db9b9260e177fb6d7e8affc059472a7ed
cultist <75032462+dairycultist@users.noreply.github.com>
2025-06-10 00:42:22 -0400

    Add files via upload

--- parser_expr.ml
+++ parser_expr.ml
@@ -1,3 +1,12 @@
+type expr =
+  | Var of string
+  | Value of string
+  (* | Add of expr * expr *)
+
+type expr_result = token list * expr
+
+
+
 let parse_expr toks : expr_result = (* good for now, will need to update once operators are added! *)
   let tok = lookahead toks in
   let toks = consume_token toks in
--- parser_stmt.ml
+++ parser_stmt.ml
@@ -1,3 +1,13 @@
+type stmt =
+  | Hostname of expr
+  | Port of expr
+  | OnRequest of expr * expr * expr * stmt list
+  | Print of expr
+
+type stmt_result = token list * stmt list
+
+
+
 let rec parse_stmt toks : stmt_result =
   if toks = [] then
     ([], [])
@@ -5,6 +15,7 @@ let rec parse_stmt toks : stmt_result =
     let (toks, stmt_list1) = (
       match lookahead toks with
       | Tok_Hostname      -> parse_stmt_hostname toks
+      | Tok_Port          -> parse_stmt_port toks
       | _ -> raise (InvalidInputException("Came across a non-statement while parsing statements!"))
     ) in
     let (toks, stmt_list2) = parse_stmt toks in
@@ -15,3 +26,9 @@ and parse_stmt_hostname toks : stmt_result =
   let (toks, expr) = parse_expr toks in
   let toks = match_token toks Tok_Semicolon in
   (toks, [ Hostname(expr) ])
+
+and parse_stmt_port toks : stmt_result =
+  let toks = match_token toks Tok_Port in
+  let (toks, expr) = parse_expr toks in
+  let toks = match_token toks Tok_Semicolon in
+  (toks, [ Port(expr) ])
