e595c85d30c239b778b186306f2feed26cd6c165
cultist <75032462+dairycultist@users.noreply.github.com>
2025-06-09 23:23:51 -0400

    Add files via upload

--- main.ml
+++ main.ml
@@ -1,8 +1,9 @@
 #use "tokenizer.ml"
 
-let read_whole_file filename =
-    (* open_in_bin works correctly on Unix and Windows *)
-    let ch = open_in_bin filename in
+let read_whole_file path =
+    let ch = open_in_bin path in
     let s = really_input_string ch (in_channel_length ch) in
     close_in ch;
     s
+
+let compile path = let _ = print_endline "doing stuff" in tokenize (read_whole_file path)
