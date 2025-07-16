18d9047a8b3f6535014928f507e4cafd1438ff47
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-07-16 18:25:14 -0400

    Simple js code

--- temp.js
+++ temp.js
@@ -3,12 +3,19 @@ const { createServer } = require('node:http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+const colorDim = "\x1b[2m";
+const colorReset = "\x1b[0m";
+const colorValue = "\x1b[32m";
+const colorError = "\x1b[41m";
+
 const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+
+    console.log(`${colorDim}[200]: ${colorReset + colorValue}GET /${colorReset}`);
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`${colorDim}Started WSAS (a1.0) server @ ${colorReset + colorValue}http://${hostname}:${port}/${colorReset}`);
 });
