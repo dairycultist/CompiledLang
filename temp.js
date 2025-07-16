cf6499a8b7ae9c8b0debc46153f42a29ac600560
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-07-16 19:07:40 -0400

    Simple js code 3

--- temp.js
+++ temp.js
@@ -6,16 +6,28 @@ const port = 3000;
 const colorDim = "\x1b[2m";
 const colorReset = "\x1b[0m";
 const colorValue = "\x1b[32m";
-const colorError = "\x1b[41m";
+const colorError = "\x1b[31m";
 
 const server = createServer((req, res) => {
-    res.statusCode = 200;
+
+    console.log(`${colorDim}=> ${colorReset + colorValue}${req.method} ${req.url}${colorReset}`);
+    
+    res.statusCode = 404;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
 
-    console.log(`${colorDim}[200]: ${colorReset + colorValue}${req.method} ${req.url}${colorReset}`);
+    switch (res.statusCode) {
+
+        case 200:
+            console.log(`${colorDim}<= [200]${colorReset}`);
+            break;
+        
+        default:
+            console.log(`${colorDim}<= [${colorReset + colorError}${res.statusCode}${colorReset + colorDim}]: ${colorReset + colorError}<Error message>${colorReset}`);
+    }
 });
 
 server.listen(port, hostname, () => {
+
     console.log(`${colorDim}Started WSAS (a1.0) server @ ${colorReset + colorValue}http://${hostname}:${port}/${colorReset}`);
 });
