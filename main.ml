665ff3c76e8af6155d7e56f1dec8c085bbb8d18a
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-07-16 23:53:36 -0400

    Compiler writes to file

--- main.ml
+++ main.ml
@@ -11,17 +11,20 @@
 *)
 
 let read_whole_file path : string =
-    let ch = open_in_bin path in
-    let s = really_input_string ch (in_channel_length ch) in
-    close_in ch;
-    s
+  let ch = open_in_bin path in
+  let s = really_input_string ch (in_channel_length ch) in
+  close_in ch;
+  s
 
-(* let save_str_to_file path str = *)
+let save_str_to_file path str : unit =
+  let oc = open_out path in
+  Printf.fprintf oc "%s" str;
+  close_out oc
 
 let compile in_path out_path : unit =
-    let _ = print_endline ("Compiled to " ^ out_path) in
-    let _ = print_endline (eval (parse (tokenize (read_whole_file in_path)))) in 
-    ()
+  let _ = print_endline ("Compiled to " ^ out_path) in
+  let _ = save_str_to_file out_path (eval (parse (tokenize (read_whole_file in_path)))) in 
+  ()
 
 (* run when compiled program is run through the commandline *)
 let () =
